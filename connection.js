e8490cb809da64ba6185233c50dfa46ba23c2663
44m1r <151672453+44m1r@users.noreply.github.com>
2023-12-19 22:51:14 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,20 @@
+import sql from "mysql"
+
+const connection = sql.createConnection({
+  host: 'mysql-5abcd1d-codingonly41-2cf1.a.aivencloud.com',
+  user: 'avnadmin',
+  password: 'AVNS_vVm81iIwC5wWV97y27i',
+  database: 'defaultdb',
+  port:10027
+});
+  
+  // Connect to MySQL
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err.stack);
+      return;
+    }
+    console.log('Connected to MySQL as id', connection.threadId);
+  });
+
+  export default connection;
